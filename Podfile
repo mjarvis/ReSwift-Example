42f10ac0a5a4fcbfb885167a7e7037aa40426a3a
Malcolm Jarvis <malcolm@metalab.co>
2018-05-02 09:06:48 -0700

    Initial Project Setup

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '11.2'
+
+target 'ReSwift-Example' do
+	pod 'ReSwift'
+end
---
+++ ReSwift-Example/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  ReSwift-Example
+//
+//  Created by Malcolm Jarvis on 2018-05-02.
+//  Copyright © 2018 ReSwift. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+
+        self.window = window
+
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
+
